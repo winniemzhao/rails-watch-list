69ad832f6f87152cb50db910108a00c5a3c8e1bd
Winnie Zhao <winniemaizhao@gmail.com>
2022-08-11 14:56:29 -0400

    created bookmarks controller and implemented all user stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   get 'lists/index'
   get 'lists/show'
   get 'lists/new'
@@ -8,5 +11,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
