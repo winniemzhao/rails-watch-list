c27bd260a9c68ccaf2bb09622ce7c2ec0d007edb
Winnie Zhao <winniemaizhao@gmail.com>
2022-08-11 12:22:00 -0400

    created list controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists, only: %i[index show new create]
 end
